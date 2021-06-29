30459450bb1bb7722df769489a0c8fe604cc7834
Francisco Cuenya Folgar <francisco.cuenya@gmail.com>
2021-06-29 10:50:44 +0200

    Replaced ES version to 5.6.0 using the right sha and removed deprecation date

--- Formula/elasticsearch-telstar.rb
+++ Formula/elasticsearch-telstar.rb
@@ -1,14 +1,16 @@
 class ElasticsearchTelstar < Formula
   desc "Distributed search & analytics engine"
   homepage "https://www.elastic.co/products/elasticsearch"
-  url "https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.6.16.tar.gz"
-  sha256 "6b035a59337d571ab70cea72cc55225c027ad142fbb07fd8984e54261657c77f"
+  # url "https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.6.16.tar.gz"
+  # sha256 "6b035a59337d571ab70cea72cc55225c027ad142fbb07fd8984e54261657c77f"
+  url "https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.6.0.tar.gz"
+  sha256 "15a79ff7206aea581f6afae48c4637210f5b3539"
 
   bottle :unneeded
 
   keg_only :versioned_formula
 
-  deprecate! :date => "2019-03-11"
+  # deprecate! :date => "2019-03-11"
 
   depends_on :java => "1.8"
 
